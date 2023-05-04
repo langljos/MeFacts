68075528c776bdb39b4a152b5719df5c5231b1ad
langljos <langljos@gmail.com>
2023-05-03 23:23:21 -0500

    Add project files.

---
+++ MeFacts/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MeFacts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+            MeFactData fact = (MeFactData)e.SelectedItem;
+            DisplayAlert("The Fact", fact.TheFact, "Ok");
+        }
+    }
+}
